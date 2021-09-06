cf596cfb47d375925b87ebb586017b2345ebbcc8
Adrian Miozga <AdrianMiozga@outlook.com>
2021-09-06 20:44:17 +0200

    Refactor

--- app/src/main/java/org/wentura/physicalapplication/Constants.kt
+++ app/src/main/java/org/wentura/physicalapplication/Constants.kt
@@ -9,7 +9,9 @@ object Constants {
     const val DARK_MODE_KEY = "darkMode"
     const val MILES_KEY = "miles"
 
-    // Meters per second to ...
+    /** Meters per second to miles per hour **/
     const val MPH = 2.23694
+
+    /** Meters per second to kilometers per hour **/
     const val KMH = 3.6
 }

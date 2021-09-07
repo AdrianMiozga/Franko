dbeca7ff78a2e8b057843cf12fa73b6abf199e12
Adrian Miozga <AdrianMiozga@outlook.com>
2021-09-07 12:27:27 +0200

    Add People screen

--- app/src/main/java/org/wentura/physicalapplication/people/PeopleFragment.kt
+++ app/src/main/java/org/wentura/physicalapplication/people/PeopleFragment.kt
@@ -1,21 +1,21 @@
-package org.wentura.physicalapplication.statistics
+package org.wentura.physicalapplication.people
 
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
-import org.wentura.physicalapplication.databinding.FragmentStatisticsBinding
+import org.wentura.physicalapplication.databinding.FragmentPeopleBinding
 
-class StatisticsFragment : Fragment() {
-    private var _binding: FragmentStatisticsBinding? = null
+class PeopleFragment : Fragment() {
+    private var _binding: FragmentPeopleBinding? = null
 
     // This property is only valid between onCreateView and
     // onDestroyView.
     private val binding get() = _binding!!
 
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View {
-        _binding = FragmentStatisticsBinding.inflate(inflater, container, false)
+        _binding = FragmentPeopleBinding.inflate(inflater, container, false)
         val view = binding.root
 
         return view

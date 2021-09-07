d3d360a220becb2afc811d3cbeb978d4202ebed9
Adrian Miozga <AdrianMiozga@outlook.com>
2021-09-07 15:19:00 +0200

    Display profile pictures

--- app/src/main/java/org/wentura/physicalapplication/people/PeopleAdapter.kt
+++ app/src/main/java/org/wentura/physicalapplication/people/PeopleAdapter.kt
@@ -3,8 +3,11 @@ package org.wentura.physicalapplication.people
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
+import coil.load
+import coil.transform.CircleCropTransformation
 import org.wentura.physicalapplication.R
 import org.wentura.physicalapplication.User
 
@@ -13,8 +16,21 @@ class PeopleAdapter(private val peoples: List<User>) :
 
     class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         private val name: TextView = view.findViewById(R.id.name)
+        private val profilePicture: ImageView = view.findViewById(R.id.profile_picture)
 
         fun bindView(position: Int, dataSet: List<User>) {
+            val uri = dataSet[position].photoUrl
+
+            if (uri == null) {
+                profilePicture.load(R.drawable.profile_picture_placeholder) {
+                    transformations(CircleCropTransformation())
+                }
+            } else {
+                profilePicture.load(uri) {
+                    transformations(CircleCropTransformation())
+                }
+            }
+
             name.text = dataSet[position].name
         }
     }

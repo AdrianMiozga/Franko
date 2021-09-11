6866b8d1a50c1252ca3d510490ece88d9d88817f
Adrian Miozga <AdrianMiozga@outlook.com>
2021-09-11 15:57:02 +0200

    Ask for unsaved changes in profile edition

--- app/src/main/java/org/wentura/physicalapplication/MainActivity.kt
+++ app/src/main/java/org/wentura/physicalapplication/MainActivity.kt
@@ -3,10 +3,8 @@ package org.wentura.physicalapplication
 import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
-import androidx.navigation.ui.navigateUp
 import androidx.navigation.ui.setupActionBarWithNavController
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.FirebaseAuthUIActivityResultContract
@@ -46,9 +44,8 @@ class MainActivity : AppCompatActivity() {
     }
 
     override fun onSupportNavigateUp(): Boolean {
-        val navController = findNavController(R.id.main_fragment_container_view)
-        return navController.navigateUp(appBarConfiguration)
-                || super.onSupportNavigateUp()
+        onBackPressedDispatcher.onBackPressed()
+        return super.onSupportNavigateUp()
     }
 
     private fun createSignInIntent() {

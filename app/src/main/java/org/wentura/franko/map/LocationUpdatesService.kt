3835c5ba9d44cbaf46600751ccc0ee1c615ef311
Adrian Miozga <AdrianMiozga@outlook.com>
2021-09-13 21:51:07 +0200

    Add notification when recording activity

--- app/src/main/java/org/wentura/franko/map/LocationUpdatesService.kt
+++ app/src/main/java/org/wentura/franko/map/LocationUpdatesService.kt
@@ -6,13 +6,18 @@ import android.app.Service
 import android.content.Intent
 import android.os.IBinder
 import android.os.Looper
+import android.os.SystemClock
 import androidx.core.app.NotificationCompat
+import androidx.core.app.NotificationManagerCompat
 import com.google.android.gms.location.FusedLocationProviderClient
 import com.google.android.gms.location.LocationRequest
 import com.google.android.gms.location.LocationServices
 import org.wentura.franko.Constants
 import org.wentura.franko.MainActivity
 import org.wentura.franko.R
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.concurrent.timerTask
 
 class LocationUpdatesService : Service() {
 
@@ -21,6 +26,11 @@ class LocationUpdatesService : Service() {
     }
 
     private lateinit var fusedLocationClient: FusedLocationProviderClient
+
+    private lateinit var timer: Timer
+    private var initialTime = -1L
+
+    private lateinit var notification: NotificationCompat.Builder
 
     private val locationRequest = LocationRequest.create().apply {
         interval = 10000
@@ -39,15 +49,17 @@ class LocationUpdatesService : Service() {
                 PendingIntent.getActivity(this, 0, notificationIntent, 0)
             }
 
-        val notification = NotificationCompat.Builder(this, Constants.ACTIVITY_RECORDING_NOTIFICATION_CHANNEL_ID)
+        notification = NotificationCompat
+            .Builder(this, Constants.ACTIVITY_RECORDING_NOTIFICATION_CHANNEL_ID)
             .setSmallIcon(R.mipmap.ic_launcher)
             .setContentTitle(getString(R.string.activity_recording_notification_title))
             .setContentIntent(pendingIntent)
             .setPriority(NotificationCompat.PRIORITY_LOW)
             .setShowWhen(false)
-            .build()
 
-        startForeground(Constants.ACTIVITY_TRACKING_NOTIFICATION_ID, notification)
+        startForeground(Constants.ACTIVITY_TRACKING_NOTIFICATION_ID, notification.build())
+
+        startTimer()
 
         fusedLocationClient.requestLocationUpdates(
             locationRequest,
@@ -61,7 +73,30 @@ class LocationUpdatesService : Service() {
     override fun onDestroy() {
         super.onDestroy()
 
+        timer.cancel()
         fusedLocationClient.removeLocationUpdates(LocationRepository)
+    }
+
+    private fun startTimer() {
+        initialTime = SystemClock.elapsedRealtime()
+
+        timer = Timer()
+        timer.scheduleAtFixedRate(timerTask {
+            val elapsedSeconds = SystemClock.elapsedRealtime() - initialTime
+
+            val title = getString(
+                R.string.activity_recording_notification_title,
+                SimpleDateFormat("mm:ss", Locale.US)
+                    .format(elapsedSeconds)
+            )
+
+            notification.setContentTitle(title)
+
+            NotificationManagerCompat
+                .from(applicationContext)
+                .notify(Constants.ACTIVITY_TRACKING_NOTIFICATION_ID, notification.build())
+
+        }, 0, 1000)
     }
 
     override fun onBind(intent: Intent?): IBinder? = null
---
+++ app/src/main/java/org/wentura/franko/map/TimerViewModel.kt
@@ -0,0 +1,46 @@
+package org.wentura.franko.map
+
+import android.os.SystemClock
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import java.text.SimpleDateFormat
+import java.util.*
+import kotlin.concurrent.timerTask
+
+class TimerViewModel : ViewModel() {
+
+    companion object {
+        val TAG = TimerViewModel::class.simpleName
+    }
+
+    private lateinit var timer: Timer
+    private var initialTime = 0L
+
+    private val _secondsElapsed = MutableLiveData<String>()
+    val secondsElapsed: LiveData<String> = _secondsElapsed
+
+    fun startTimer() {
+        initialTime = SystemClock.elapsedRealtime()
+
+        timer = Timer()
+        timer.scheduleAtFixedRate(timerTask {
+            val newValue = SystemClock.elapsedRealtime() - initialTime
+
+            _secondsElapsed.postValue(
+                SimpleDateFormat("mm:ss", Locale.US)
+                    .format(newValue)
+            )
+        }, 0, 1000)
+    }
+
+    fun stopTimer() {
+        _secondsElapsed.postValue("")
+        timer.cancel()
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        timer.cancel()
+    }
+}

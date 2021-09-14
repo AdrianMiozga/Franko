2de4fa46e729f287f530a2fa25fbac7f84039f4f
Adrian Miozga <AdrianMiozga@outlook.com>
2021-09-14 18:30:04 +0200

    Listen to changes of paths

--- app/src/main/java/org/wentura/franko/activities/ActivityListViewModel.kt
+++ app/src/main/java/org/wentura/franko/activities/ActivityListViewModel.kt
@@ -1,12 +1,11 @@
 package org.wentura.franko.activities
 
+import android.util.Log
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.viewModelScope
 import com.google.firebase.firestore.ktx.toObjects
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.launch
 import org.wentura.franko.data.Path
 import javax.inject.Inject
 
@@ -15,12 +14,26 @@ class ActivityListViewModel @Inject constructor(
     private val activityRepository: ActivityRepository
 ) : ViewModel() {
 
-    private val _paths = MutableLiveData<ArrayList<Path>>()
-    val paths: LiveData<ArrayList<Path>> = _paths
+    companion object {
+        val TAG = ActivityListViewModel::class.simpleName
+    }
 
-    init {
-        viewModelScope.launch {
-            _paths.value = ArrayList(activityRepository.getPaths().toObjects())
-        }
+    private val paths = MutableLiveData<ArrayList<Path>>()
+
+    fun getCurrentPaths(): LiveData<ArrayList<Path>> {
+        activityRepository
+            .getPaths()
+            .addSnapshotListener { querySnapshot, exception ->
+                if (exception != null) {
+                    Log.w(TAG, "Listen failed.", exception)
+                    return@addSnapshotListener
+                }
+
+                if (querySnapshot == null || querySnapshot.isEmpty) return@addSnapshotListener
+
+                paths.value = ArrayList(querySnapshot.toObjects())
+            }
+
+        return paths
     }
 }

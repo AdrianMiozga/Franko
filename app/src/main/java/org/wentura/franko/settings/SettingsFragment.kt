8daa6c4d67cf3c7bd55839e768081c75c92e8965
Adrian Miozga <AdrianMiozga@outlook.com>
2021-09-16 15:48:54 +0200

    Make SettingsFragment use UserRepository

--- app/src/main/java/org/wentura/franko/settings/SettingsFragment.kt
+++ app/src/main/java/org/wentura/franko/settings/SettingsFragment.kt
@@ -1,22 +1,22 @@
 package org.wentura.franko.settings
 
 import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.viewModels
 import androidx.preference.ListPreference
 import androidx.preference.PreferenceFragmentCompat
 import androidx.preference.SwitchPreferenceCompat
-import com.google.firebase.auth.FirebaseAuth
-import com.google.firebase.firestore.ktx.firestore
-import com.google.firebase.firestore.ktx.toObject
-import com.google.firebase.ktx.Firebase
+import dagger.hilt.android.AndroidEntryPoint
 import org.wentura.franko.Constants
 import org.wentura.franko.R
-import org.wentura.franko.data.User
+import org.wentura.franko.viewmodels.UserViewModel
 
+@AndroidEntryPoint
 class SettingsFragment : PreferenceFragmentCompat() {
 
-    private val db = Firebase.firestore
-
-    private val uid = FirebaseAuth.getInstance().currentUser?.uid ?: ""
+    private val userViewModel: UserViewModel by viewModels()
 
     companion object {
         val TAG = SettingsFragment::class.simpleName
@@ -26,7 +26,9 @@ class SettingsFragment : PreferenceFragmentCompat() {
         setPreferencesFromResource(R.xml.preferences, rootKey)
 
         preferenceManager.preferenceDataStore = DataStore()
+    }
 
+    override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
         val darkModeSwitch: SwitchPreferenceCompat? = preferenceManager.findPreference(Constants.DARK_MODE_KEY)
         val unitsOfMeasure: ListPreference? = preferenceManager.findPreference(Constants.UNITS_OF_MEASURE_KEY)
         val whoCanSeeMyProfile: ListPreference? = preferenceManager.findPreference(Constants.WHO_CAN_SEE_MY_PROFILE)
@@ -34,43 +36,40 @@ class SettingsFragment : PreferenceFragmentCompat() {
         val whoCanSeeMyFollowingCount: ListPreference? =
             preferenceManager.findPreference(Constants.WHO_CAN_SEE_MY_FOLLOWING_COUNT)
 
-        db.collection(Constants.USERS)
-            .document(uid)
-            .get()
-            .addOnSuccessListener { result ->
-                val user = result.toObject<User>() ?: return@addOnSuccessListener
-
-                darkModeSwitch?.let {
-                    it.isChecked = user.darkMode ?: false
-                }
-
-                unitsOfMeasure?.let {
-                    val array = resources.getStringArray(R.array.units_of_measure_array)
-                    val index = array.indexOf(user.unitsOfMeasure)
-
-                    unitsOfMeasure.setValueIndex(if (index == -1) 0 else index)
-                }
-
-                whoCanSeeMyProfile?.let {
-                    val array = resources.getStringArray(R.array.who_can_see_my_profile)
-                    val index = array.indexOf(user.whoCanSeeMyProfile)
-
-                    whoCanSeeMyProfile.setValueIndex(if (index == -1) 0 else index)
-                }
-
-                whoCanSeeMyLocation?.let {
-                    val array = resources.getStringArray(R.array.who_can_see_my_location)
-                    val index = array.indexOf(user.whoCanSeeMyLocation)
-
-                    whoCanSeeMyLocation.setValueIndex(if (index == -1) 0 else index)
-                }
-
-                whoCanSeeMyFollowingCount?.let {
-                    val array = resources.getStringArray(R.array.who_can_see_my_following_count)
-                    val index = array.indexOf(user.whoCanSeeMyFollowingCount)
-
-                    whoCanSeeMyFollowingCount.setValueIndex(if (index == -1) 0 else index)
-                }
+        userViewModel.getUser().observe(viewLifecycleOwner) { user ->
+            darkModeSwitch?.let {
+                it.isChecked = user.darkMode ?: false
             }
+
+            unitsOfMeasure?.let {
+                val array = resources.getStringArray(R.array.units_of_measure_array)
+                val index = array.indexOf(user.unitsOfMeasure)
+
+                unitsOfMeasure.setValueIndex(if (index == -1) 0 else index)
+            }
+
+            whoCanSeeMyProfile?.let {
+                val array = resources.getStringArray(R.array.who_can_see_my_profile)
+                val index = array.indexOf(user.whoCanSeeMyProfile)
+
+                whoCanSeeMyProfile.setValueIndex(if (index == -1) 0 else index)
+            }
+
+            whoCanSeeMyLocation?.let {
+                val array = resources.getStringArray(R.array.who_can_see_my_location)
+                val index = array.indexOf(user.whoCanSeeMyLocation)
+
+                whoCanSeeMyLocation.setValueIndex(if (index == -1) 0 else index)
+            }
+
+            whoCanSeeMyFollowingCount?.let {
+                val array = resources.getStringArray(R.array.who_can_see_my_following_count)
+                val index = array.indexOf(user.whoCanSeeMyFollowingCount)
+
+                whoCanSeeMyFollowingCount.setValueIndex(if (index == -1) 0 else index)
+            }
+        }
+
+        return super.onCreateView(inflater, container, savedInstanceState)
     }
 }

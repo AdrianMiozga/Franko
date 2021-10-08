688ed36d05ce65d1acf9b6de8e65f0d5f0908845
Adrian Miozga <AdrianMiozga@outlook.com>
2021-10-08 12:27:30 +0200

    Improve UI

--- app/src/main/java/org/wentura/franko/adapters/ProfilePagerAdapter.kt
+++ app/src/main/java/org/wentura/franko/adapters/ProfilePagerAdapter.kt
@@ -4,17 +4,14 @@ import androidx.fragment.app.Fragment
 import androidx.viewpager2.adapter.FragmentStateAdapter
 import org.wentura.franko.Constants.ACTIVITIES_PAGE_INDEX
 import org.wentura.franko.Constants.PROFILE_PAGE_INDEX
-import org.wentura.franko.Constants.SETTINGS_PAGE_INDEX
 import org.wentura.franko.activities.ActivitiesFragment
 import org.wentura.franko.profile.ProfileFragment
-import org.wentura.franko.settings.SettingsFragment
 
 class ProfilePagerAdapter(fragment: Fragment) : FragmentStateAdapter(fragment) {
 
     private val tabFragmentsCreators: Map<Int, () -> Fragment> = mapOf(
-        ACTIVITIES_PAGE_INDEX to { ActivitiesFragment() },
         PROFILE_PAGE_INDEX to { ProfileFragment() },
-        SETTINGS_PAGE_INDEX to { SettingsFragment() }
+        ACTIVITIES_PAGE_INDEX to { ActivitiesFragment() }
     )
 
     override fun getItemCount() = tabFragmentsCreators.size

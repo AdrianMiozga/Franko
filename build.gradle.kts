78c225354f5b99f29ee7dcd9a906bf4de4f8f70b
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-23 22:57:05 +0100

    Update Gradle plugins

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,16 +1,16 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.8.0" apply false
+    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
 
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
-    id("com.google.gms.google-services") version "4.3.15" apply false
-    id("androidx.navigation.safeargs.kotlin") version "2.5.3" apply false
+    id("com.google.gms.google-services") version "4.4.0" apply false
+    id("androidx.navigation.safeargs.kotlin") version "2.7.6" apply false
 
     // JUnit 5
-    id("de.mannodermaus.android-junit5") version "1.8.2.1" apply false
+    id("de.mannodermaus.android-junit5") version "1.10.0.0" apply false
 
     // Hilt
-    id("com.google.dagger.hilt.android") version "2.45" apply false
+    id("com.google.dagger.hilt.android") version "2.50" apply false
 }
 
 tasks.register("clean", Delete::class) {

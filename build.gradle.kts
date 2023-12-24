8264ec5e18ef79c0f6f17640e21a39a27a3b2bdd
Adrian Miozga <AdrianMiozga@outlook.com>
2023-12-24 01:28:54 +0100

    Update

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,6 +1,9 @@
 plugins {
     id("com.android.application") version "8.2.0" apply false
-    id("org.jetbrains.kotlin.android") version "1.9.22" apply false
+
+    val kotlinVersion = "1.9.22"
+    id("org.jetbrains.kotlin.android") version kotlinVersion apply false
+    id("org.jetbrains.kotlin.kapt") version kotlinVersion apply false
 
     id("com.google.android.libraries.mapsplatform.secrets-gradle-plugin") version "2.0.1" apply false
     id("com.google.gms.google-services") version "4.4.0" apply false
